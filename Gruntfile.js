bf270aca79b2296a39fdb3c98f2d6bf77e2e52be
MaxMEllon <mozi_kke@excite.co.jp>
2015-01-20 20:53:54 +0900

    edit: テンプレート作成

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    plg: grunt.file.readJSON('package.json'),
+
     sass: {
       build: {
         src: "src/scss/*.scss",
@@ -8,24 +10,40 @@ module.exports = function(grunt) {
     },
     slim: {
       build: {
+        src: "src/slim/index.slim",
+        dest: 'build/view/index.html'
       }
     },
     coffee: {
       build: {
-        // files: {
-        //   // 'src/coffee/*.coffee': 'src/coffee/*.coffee'
-        // }
       }
     },
     csslint: {
       check: {
         src: '<%= sass.build.dest>'
       }
+    },
+    watch: {
+      files: 'src/**/*',
+      tasks: ['sass', 'slim', 'csslint']
+    },
+    connect: {
+      server: {
+        options: {
+          port: 3000
+        }
+      }
     }
   });
-  grunt.loadNpmTasks('grunt-contrib-sass')
-  grunt.loadNpmTasks('grunt-contrib-coffee')
-  grunt.loadNpmTasks('grunt-slim')
-  grunt.loadNpmTasks('grunt-contrib-csslint')
-  grunt.registerTask('default', ['sass', 'csslint'])
+
+  // plugin
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-slim');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  //tasks
+  grunt.registerTask('default', ['sass', 'slim', 'csslint', 'connect', 'watch']);
 }
